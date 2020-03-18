4f607b0b82b7d8be5f2ae99e45f10cb710698916
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-03-18 22:46:26 +0900

    refactor: format

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+/* Application-wide Styles */
+h1 {
+  color: #369;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 250%;
 }
-
-#nav {
-  padding: 30px;
+h2,
+h3 {
+  color: #444;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: lighter;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+body {
+  margin: 2em;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body,
+input[type='text'],
+button {
+  color: #333;
+  font-family: Cambria, Georgia;
+}
+/* everywhere else */
+* {
+  font-family: Arial, Helvetica, sans-serif;
 }
 </style>
