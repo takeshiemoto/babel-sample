e90598d189834ed81427da8b770c847089383ea8
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-04-05 00:00:43 +0900

    feat: Counter App Fix

--- src/views/Counter.vue
+++ src/views/Counter.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="counter">
-    <h2>Counter App</h2>
+    <div>{{ count }}</div>
+    <v-btn @click="increment()">+</v-btn>
+    <v-btn @click="decrement()">-</v-btn>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
+interface CounterData {
+  count: number;
+}
+
 export default Vue.extend({
   name: 'Counter',
+  data(): CounterData {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement(): void {
+      this.count--;
+    },
+  },
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.counter {
+  padding: 20px;
+}
+</style>
